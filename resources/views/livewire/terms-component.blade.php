7c660852baffb69f529fe2d3d5cac03657108fe2
petermuremi351 <mcompsnetwork@gmail.com>
2024-06-25 00:38:41 +0300

    terms and condition updates
    Co-Authored-By: Muremi Peter <116870103+petermuremi351@users.noreply.github.com>

--- resources/views/livewire/terms-component.blade.php
+++ resources/views/livewire/terms-component.blade.php
@@ -25,34 +25,22 @@
     <div class="rules-area ptb-100">
         <div class="container">
             <div class="rules-item">
-                <h3>1 - Agreement of user</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas porro amet consequuntur in
-                    voluptatum perspiciatis, nostrum deserunt quos accusamus, eos optio recusandae eius facilis
-                    voluptatibus expedita numquam eaque praesentium laboriosam.</p>
-                <h3>2 - Access and use of the site</h3>
+                <h3>1 - Agreement of User</h3>
+                <p>By accessing and using this website, you agree to comply with and be bound by the following terms and conditions, which together with our Privacy Policy govern [Your Company Name]'s relationship with you in relation to this website.</p>
+                <h3>2 - Access and Use of the Site</h3>
                 <ul>
-                    <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry</li>
-                    <li>It is a long established fact that a reader will be distracted by the readable</li>
-                    <li>Contrary to popular belief, Lorem Ipsum is not simply random text</li>
-                    <li>There are many variations of passages of Lorem Ipsum available</li>
-                    <li>The standard chunk of Lorem Ipsum used since the 1500s is reproduced</li>
+                    <li>Unauthorized use of this website may give rise to a claim for damages and/or be a criminal offense.</li>
+                    <li>From time to time, this website may also include links to other websites. These links are provided for your convenience to provide further information.</li>
+                    <li>Your use of any information or materials on this website is entirely at your own risk, for which we shall not be liable.</li>
+                    <li>This website contains material which is owned by or licensed to us. This material includes, but is not limited to, the design, layout, look, appearance, and graphics.</li>
+                    <li>All trademarks reproduced in this website, which are not the property of, or licensed to the operator, are acknowledged on the website.</li>
                 </ul>
-                <h3>3 - Copyright infringement</h3>
-                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
-                    classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin
-                    professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words,
-                    consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical
-                    literature, discovered the undoubtable source.</p>
-                <h3>4 - Access and interference</h3>
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered
-                    alteration in some form, by injected humour, or randomised words which don't look even slightly
-                    believable. If you are going to use a passage of Lorem Ipsum. But the majority have suffered
-                    alteration in some form, by injected humour.</p>
-                <h3>5 - Limitation of liability</h3>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-                    leap into electronic typesetting.</p>
+                <h3>3 - Copyright Infringement</h3>
+                <p>If you believe that any content appearing on this website infringes on your copyright, please contact us at [Your Contact Information] with a written notice containing the following information:</p>
+                <h3>4 - Access and Interference</h3>
+                <p>You agree that you will not use any robot, spider, scraper, deep link or other similar automated data gathering or extraction tools, program, algorithm or methodology to access, acquire, copy or monitor the website or any portion of the website or for any other purpose, without our prior written permission.</p>
+                <h3>5 - Limitation of Liability</h3>
+                <p>In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.</p>
             </div>
         </div>
     </div>
