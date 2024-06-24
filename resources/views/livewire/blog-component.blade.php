2fbeb3751eba6675e75bfa5b709c4dbb432ef127
petermuremi351 <mcompsnetwork@gmail.com>
2024-06-24 16:08:35 +0300

    blog page
    Co-Authored-By: Muremi Peter <116870103+petermuremi351@users.noreply.github.com>

--- resources/views/livewire/blog-component.blade.php
+++ resources/views/livewire/blog-component.blade.php
@@ -1,3 +1,218 @@
 <div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+
+    <div class="page-title-area title-bg-six">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="container">
+                    <div class="title-item">
+                        <h2>Blog</h2>
+                        <ul>
+                            <li>
+                                <a href="/">Home</a>
+                            </li>
+                            <li>
+                                <span>Blog</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <section class="blog-area three ptb-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-lg-4">
+                    <div class="blog-item">
+                        <div class="top">
+                            <a href="{{ route('blog.details') }}">
+                                <img src="{{ asset('assets/img/blog/blog1.jpg') }}" alt="Blog">
+                            </a>
+                        </div>
+                        <div class="bottom">
+                            <ul>
+                                <li>
+                                    <i class="icofont-calendar"></i>
+                                    <span>21 Jan, 2024</span>
+                                </li>
+                                <li>
+                                    <i class="icofont-user-alt-3"></i>
+                                    <span>By:</span>
+                                    <a href="#">Admin</a>
+                                </li>
+                            </ul>
+                            <h3>
+                                <a href="{{ route('blog.details') }}">Donate for nutration less poor people</a>
+                            </h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet cupiditate sit ducimus
+                                dolor laudantium distinction</p>
+                            <a class="blog-btn" href="{{ route('blog.details') }}">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4">
+                    <div class="blog-item">
+                        <div class="top">
+                            <a href="{{ route('blog.details') }}">
+                                <img src="{{ asset('assets/img/blog/blog2.jpg') }}" alt="Blog">
+                            </a>
+                        </div>
+                        <div class="bottom">
+                            <ul>
+                                <li>
+                                    <i class="icofont-calendar"></i>
+                                    <span>22 Jan, 2024</span>
+                                </li>
+                                <li>
+                                    <i class="icofont-user-alt-3"></i>
+                                    <span>By:</span>
+                                    <a href="#">Admin</a>
+                                </li>
+                            </ul>
+                            <h3>
+                                <a href="{{ route('blog.details') }}">Charity meetup in Berline next year</a>
+                            </h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet cupiditate sit ducimus
+                                dolor laudantium distinction</p>
+                            <a class="blog-btn" href="{{ route('blog.details') }}">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4">
+                    <div class="blog-item">
+                        <div class="top">
+                            <a href="{{ route('blog.details') }}">
+                                <img src="{{ asset('assets/img/blog/blog3.jpg') }}" alt="Blog">
+                            </a>
+                        </div>
+                        <div class="bottom">
+                            <ul>
+                                <li>
+                                    <i class="icofont-calendar"></i>
+                                    <span>23 Jan, 2024</span>
+                                </li>
+                                <li>
+                                    <i class="icofont-user-alt-3"></i>
+                                    <span>By:</span>
+                                    <a href="#">Admin</a>
+                                </li>
+                            </ul>
+                            <h3>
+                                <a href="{{ route('blog.details') }}">Donate for the poor people to help them</a>
+                            </h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet cupiditate sit ducimus
+                                dolor laudantium distinction</p>
+                            <a class="blog-btn" href="{{ route('blog.details') }}">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4">
+                    <div class="blog-item">
+                        <div class="top">
+                            <a href="{{ route('blog.details') }}">
+                                <img src="{{ asset('assets/img/blog/blog4.jpg') }}" alt="Blog">
+                            </a>
+                        </div>
+                        <div class="bottom">
+                            <ul>
+                                <li>
+                                    <i class="icofont-calendar"></i>
+                                    <span>24 Jan, 2024</span>
+                                </li>
+                                <li>
+                                    <i class="icofont-user-alt-3"></i>
+                                    <span>By:</span>
+                                    <a href="#">Admin</a>
+                                </li>
+                            </ul>
+                            <h3>
+                                <a href="{{ route('blog.details') }}">Charity projects for over 10 million people</a>
+                            </h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet cupiditate sit ducimus
+                                dolor laudantium distinction</p>
+                            <a class="blog-btn" href="{{ route('blog.details') }}">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4">
+                    <div class="blog-item">
+                        <div class="top">
+                            <a href="{{ route('blog.details') }}">
+                                <img src="{{ asset('assets/img/blog/blog5.jpg') }}" alt="Blog">
+                            </a>
+                        </div>
+                        <div class="bottom">
+                            <ul>
+                                <li>
+                                    <i class="icofont-calendar"></i>
+                                    <span>25 Jan, 2024</span>
+                                </li>
+                                <li>
+                                    <i class="icofont-user-alt-3"></i>
+                                    <span>By:</span>
+                                    <a href="#">Admin</a>
+                                </li>
+                            </ul>
+                            <h3>
+                                <a href="{{ route('blog.details') }}">Save the environment to safe your life</a>
+                            </h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet cupiditate sit ducimus
+                                dolor laudantium distinction</p>
+                            <a class="blog-btn" href="{{ route('blog.details') }}">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4">
+                    <div class="blog-item">
+                        <div class="top">
+                            <a href="{{ route('blog.details') }}">
+                                <img src="{{ asset('assets/img/blog/blog6.jpg') }}" alt="Blog">
+                            </a>
+                        </div>
+                        <div class="bottom">
+                            <ul>
+                                <li>
+                                    <i class="icofont-calendar"></i>
+                                    <span>26 Jan, 2024</span>
+                                </li>
+                                <li>
+                                    <i class="icofont-user-alt-3"></i>
+                                    <span>By:</span>
+                                    <a href="#">Admin</a>
+                                </li>
+                            </ul>
+                            <h3>
+                                <a href="{{ route('blog.details') }}">A world where love is pure and the cure</a>
+                            </h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet cupiditate sit ducimus
+                                dolor laudantium distinction</p>
+                            <a class="blog-btn" href="{{ route('blog.details') }}">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="pagination-area">
+                <ul>
+                    <li>
+                        <a href="#">Prev</a>
+                    </li>
+                    <li>
+                        <a class="active" href="#">1</a>
+                    </li>
+                    <li>
+                        <a href="#">2</a>
+                    </li>
+                    <li>
+                        <a href="#">3</a>
+                    </li>
+                    <li>
+                        <a href="#">Next</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
 </div>
