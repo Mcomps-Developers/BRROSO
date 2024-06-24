e5140003ef1f976cd81ceb7c1b425788b0b040ea
petermuremi351 <mcompsnetwork@gmail.com>
2024-06-24 19:39:30 +0300

    donation details
    Co-Authored-By: Muremi Peter <116870103+petermuremi351@users.noreply.github.com>

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -1,71 +1,53 @@
 <div>
     <div class="banner-area-two three">
         <div class="banner-slider owl-theme owl-carousel">
-          <div class="banner-slider-item banner-img-four">
-            <div class="d-table">
-              <div class="d-table-cell">
-                <div class="container">
-                  <div class="banner-content">
-                    <h1>We help all of the people in need around the world</h1>
-                    <p>
-                      It is a long established fact that a reader will be page
-                      distracted by the readable content of a pain
-                    </p>
-                    <div class="banner-btn-area">
-                      <a class="common-btn banner-btn" href="#"
-                        >Get Start A Fundraising</a
-                      >
-                      <a class="common-btn" href="#">Donate Now</a>
+            <div class="banner-slider-item banner-img-four">
+                <div class="d-table">
+                    <div class="d-table-cell">
+                        <div class="container">
+                            <div class="banner-content">
+
+                                <div class="banner-btn-area">
+                                    <a class="common-btn banner-btn" href="#">Get Start A Fundraising</a>
+                                    <a class="common-btn" href="#">Donate Now</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
-          <div class="banner-slider-item banner-img-five">
-            <div class="d-table">
-              <div class="d-table-cell">
-                <div class="container">
-                  <div class="banner-content">
-                    <h1>Become a volunteer in our community to help people</h1>
-                    <p>
-                      It is a long established fact that a reader will be page
-                      distracted by the readable content of a pain
-                    </p>
-                    <div class="banner-btn-area">
-                      <a class="common-btn banner-btn" href="#"
-                        >Get Start A Fundraising</a
-                      >
-                      <a class="common-btn" href="#">Donate Now</a>
+            <div class="banner-slider-item banner-img-five">
+                <div class="d-table">
+                    <div class="d-table-cell">
+                        <div class="container">
+                            <div class="banner-content">
+
+                                <div class="banner-btn-area">
+                                    <a class="common-btn banner-btn" href="#">Get Start A Fundraising</a>
+                                    <a class="common-btn" href="#">Donate Now</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
-          <div class="banner-slider-item banner-img-six">
-            <div class="d-table">
-              <div class="d-table-cell">
-                <div class="container">
-                  <div class="banner-content">
-                    <h1>We are donating all over the world by our volunteer</h1>
-                    <p>
-                      It is a long established fact that a reader will be page
-                      distracted by the readable content of a pain
-                    </p>
-                    <div class="banner-btn-area">
-                      <a class="common-btn banner-btn" href="#"
-                        >Get Start A Fundraising</a
-                      >
-                      <a class="common-btn" href="#">Donate Now</a>
+            <div class="banner-slider-item banner-img-six">
+                <div class="d-table">
+                    <div class="d-table-cell">
+                        <div class="container">
+                            <div class="banner-content">
+
+                                <div class="banner-btn-area">
+                                    <a class="common-btn banner-btn" href="#">Get Start A Fundraising</a>
+                                    <a class="common-btn" href="#">Donate Now</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
+    </div>
 
 
 
@@ -142,8 +124,8 @@
                         <h3>
                             <a href="#">Core Values</a>
                         </h3>
-                        <p>Contrary to popular belief, Lorem Ipsum is not simply rom text. Contrary to popular belief is
-                            not simply.</p>
+                        <p>Respect for human dignity and rights, Accountability and transparency,
+                            Empowerment of communities, Gender equality and social justice</p>
                     </div>
                 </div>
             </div>
