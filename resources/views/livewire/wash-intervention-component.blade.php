7daa29cfe112338db1eea4bd0d378dfcc616411e
petermuremi351 <mcompsnetwork@gmail.com>
2024-06-25 13:07:16 +0300

    wash intervation page
    Co-Authored-By: Muremi Peter <116870103+petermuremi351@users.noreply.github.com>

--- resources/views/livewire/wash-intervention-component.blade.php
+++ resources/views/livewire/wash-intervention-component.blade.php
@@ -1,3 +1,192 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+
+    <div class="page-title-area title-bg-three">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="container">
+                    <div class="title-item">
+                        <h2>WASH INTERVENTION:</h2>
+                        <ul>
+                            <li>
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li>
+                                <span>Donation Details</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="donation-details-area ptb-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-13">
+                    <div class="details-item">
+                        <div class="details-img">
+
+                            <h6>1. Extension and/or improvement of water supply systems: </h6>
+                            <p>Enhancing existing water supply systems through activities such as piping, pumping
+                                systems, water tanks,
+                                and kiosks targeting communities, nutrition/health facilities.</p>
+                            <h6>2. Rehabilitation and equipping of strategic water sources:</h6>
+                            <p>Restoring and equipping strategic water sources such as pumping systems, elevated water
+                                tanks, and
+                                distribution systems/networks targeting communities, nutrition/health facilities.</p>
+                            <h6>3. Construction drilling of new strategic water sources:</h6>
+                            <p> Building new water sources targeting rural communities to ensure access to clean water.
+                                </h2>
+
+                            <h6>4. Provision of emergency water supply: </h2>
+                                <p>Providing emergency water supply targeting communities, nutrition/health facilities
+                                    during crises or when
+                                    water sources are compromised. </p>
+                                <h6>5. Water treatment and monitoring: </h6>
+                                <p>Implementing water treatment and monitoring measures, such as chlorination of
+                                    communal water sources
+                                    and household water treatment, targeting communities, nutrition/health facilities.
+                                </p>
+                                <h6>6. Construction of new emergency latrines: </h6>
+                                <p>Constructing emergency latrines with proper hand washing facilities specifically for IDPs. 
+                                </p>
+                                <h6>7. Emergency desludging of pit latrines: </h6>
+                                <p>Performing emergency desludging of pit latrines to maintain sanitation and prevent 
+                                    health risks. 
+                                </p>
+                                <h6>8. Provision of sanitation tools for solid waste management: </h6>
+                                <p>Distributing sanitation tools to support solid waste management activities such as garbage collection 
+                                    and clean campaigns for IDPs. 
+                                </p>
+                                <h6>9. Distribution of hygiene kits and hygiene promotion campaigns:</h6>
+                                <p>Providing hygiene kits, including menstrual hygiene management (MHM) sets, and conducting hygiene 
+                                    promotion campaigns to raise awareness and promote good hygiene practices in communities, 
+                                    nutrition/health facilities.
+                                </p>
+
+
+
+                        </div>
+                        <div class="details-share">
+                            <div class="row">
+                                <div class="col-sm-6 col-lg-6">
+                                    <div class="left">
+                                        <ul>
+                                            <li>
+                                                <span>Share:</span>
+                                            </li>
+                                            <li>
+                                                <a href="#" target="_blank">
+                                                    <i class="icofont-facebook"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#" target="_blank">
+                                                    <i class="icofont-twitter"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#" target="_blank">
+                                                    <i class="icofont-youtube-play"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#" target="_blank">
+                                                    <i class="icofont-instagram"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-lg-6">
+                                    <div class="right">
+                                        <ul>
+                                            <li>
+                                                <span>Tags:</span>
+                                            </li>
+                                            <li>
+                                                <a href="#">#Donation</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">#Food</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">#Help</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="details-payment">
+                            <h3>Select payment method</h3>
+                            <form>
+                                <div class="form-radio-area">
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                            id="inlineRadio1" value="option1">
+                                        <label class="form-check-label" for="inlineRadio1">Paypal</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                            id="inlineRadio2" value="option2">
+                                        <label class="form-check-label" for="inlineRadio2">Debit Card</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                            id="inlineRadio3" value="option3">
+                                        <label class="form-check-label" for="inlineRadio3">Credit Card</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                            id="inlineRadio4" value="option4">
+                                        <label class="form-check-label" for="inlineRadio4">Other</label>
+                                    </div>
+                                </div>
+                                <div class="form-input-area">
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-user-alt-3"></i>
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="First name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-user-alt-3"></i>
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="Last name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-ui-email"></i>
+                                        </label>
+                                        <input type="email" class="form-control" placeholder="Email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-ui-call"></i>
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="Phone number">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-dollar"></i>
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="$100.00">
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit" class="btn common-btn">Donate Now</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 </div>
