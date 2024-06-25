19586749504ce5509d93a9c0c46baa393ebc42d3
petermuremi351 <mcompsnetwork@gmail.com>
2024-06-25 12:31:01 +0300

    update protection intervention
    Co-Authored-By: Muremi Peter <116870103+petermuremi351@users.noreply.github.com>

--- resources/views/livewire/protection-intervention-component.blade.php
+++ resources/views/livewire/protection-intervention-component.blade.php
@@ -1,3 +1,174 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+
+    <div class="page-title-area title-bg-three">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="container">
+                    <div class="title-item">
+                        <h2>PROTECTION (GBV) INTERVENTION:</h2>
+                        <ul>
+                            <li>
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li>
+                                <span>Donation Details</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="donation-details-area ptb-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-13">
+                    <div class="details-item">
+                        <div class="details-img">
+
+                            <h6>1. Provision of clinical management of rape services: </h6>
+                            <p>Providing medical care and support to survivors of rape through existing GBV one-stop 
+                                centers and safe spaces for women and girls.</p>
+                            <h6>2. Support for specialized case management and psycho-social support:</h6>
+                            <p>Assisting survivors through specialized case management, psycho-social support, and 
+                                counseling services available at GBV one-stop centers and safe spaces.</p>
+                            <h6>3.Support for the procurement and distribution of dignity kits:</h6>
+                            <p> Ensuring the provision of dignity kits containing essential items for personal hygiene and wellbeing.</h2>
+
+                            <h6>4. Cash and vouchers assistance:</h2>
+                            <p>Providing cash and vouchers assistance to vulnerable women and girls through case 
+                                management systems and conditional cash transfers, based on the standard GBV criteria.</p>
+                            <h6>5. Provision of legal aid services and information:  </h6>
+                            <p>Offering legal aid services and information to GBV survivors, empowering them to seek 
+                                justice and protection.  </p>
+                                <h6>6. Awareness raising and outreach activities: </h6>
+                            <p>Conducting activities to raise awareness about the availability of services, prevention of GBV, and 
+                                mitigation strategies. </p>
+                                
+                                
+
+
+                        </div>
+                        <div class="details-share">
+                            <div class="row">
+                                <div class="col-sm-6 col-lg-6">
+                                    <div class="left">
+                                        <ul>
+                                            <li>
+                                                <span>Share:</span>
+                                            </li>
+                                            <li>
+                                                <a href="#" target="_blank">
+                                                    <i class="icofont-facebook"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#" target="_blank">
+                                                    <i class="icofont-twitter"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#" target="_blank">
+                                                    <i class="icofont-youtube-play"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#" target="_blank">
+                                                    <i class="icofont-instagram"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-lg-6">
+                                    <div class="right">
+                                        <ul>
+                                            <li>
+                                                <span>Tags:</span>
+                                            </li>
+                                            <li>
+                                                <a href="#">#Donation</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">#Food</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">#Help</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="details-payment">
+                            <h3>Select payment method</h3>
+                            <form>
+                                <div class="form-radio-area">
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                            id="inlineRadio1" value="option1">
+                                        <label class="form-check-label" for="inlineRadio1">Paypal</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                            id="inlineRadio2" value="option2">
+                                        <label class="form-check-label" for="inlineRadio2">Debit Card</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                            id="inlineRadio3" value="option3">
+                                        <label class="form-check-label" for="inlineRadio3">Credit Card</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                            id="inlineRadio4" value="option4">
+                                        <label class="form-check-label" for="inlineRadio4">Other</label>
+                                    </div>
+                                </div>
+                                <div class="form-input-area">
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-user-alt-3"></i>
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="First name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-user-alt-3"></i>
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="Last name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-ui-email"></i>
+                                        </label>
+                                        <input type="email" class="form-control" placeholder="Email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-ui-call"></i>
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="Phone number">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-dollar"></i>
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="$100.00">
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit" class="btn common-btn">Donate Now</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 </div>
