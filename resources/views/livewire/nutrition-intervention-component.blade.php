364409da08ee6603dc02970aa2be5c940992e65c
petermuremi351 <mcompsnetwork@gmail.com>
2024-06-25 12:17:32 +0300

    nutrition page
    Co-Authored-By: Muremi Peter <116870103+petermuremi351@users.noreply.github.com>

--- resources/views/livewire/nutrition-intervention-component.blade.php
+++ resources/views/livewire/nutrition-intervention-component.blade.php
@@ -1,3 +1,180 @@
 <div>
-    {{-- Be like water. --}}
+
+    <div class="page-title-area title-bg-three">
+        <div class="d-table">
+            <div class="d-table-cell">
+                <div class="container">
+                    <div class="title-item">
+                        <h2>HEALTH INTERVENTION:</h2>
+                        <ul>
+                            <li>
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li>
+                                <span>Donation Details</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="donation-details-area ptb-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-13">
+                    <div class="details-item">
+                        <div class="details-img">
+
+                            <h6>1. Provision of essential primary healthcare services: </h6>
+                            <p>Delivering comprehensive primary healthcare services, including clinical care, child
+                                healthcare, immunization, communicable disease treatment, TB screening and treatment,
+                                and
+                                emergency and trauma care. </p>
+                            <h6>2.Provision of essential medicines and supplies: </h6>
+                            <p>Ensuring the availability of essential medicines and medical supplies to meet the
+                                healthcare
+                                needs of the population. </p>
+                            <h6>3.Provision of sexual and reproductive health services:</h6>
+                            <p>Offering services such as antenatal care (ANC), postnatal care (PNC), safe deliveries,
+                                and
+                                dignity kits for pregnant mothers. </h2>
+
+                            <h6>4. Provision of trauma care services:</h2>
+                            <p>Providing specialized care and support to individuals who have experienced trauma or
+                                injuries.</p>
+                            <h6>5. Establishing referral pathways</h6>
+                            <p>Creating pathways for mental health and psychosocial support services to ensure 
+                                comprehensive care for individuals in need.</p>
+                                <h6>6. Infection Prevention and Control (IPC):</h6>
+                            <p>Implementing measures to prevent and control the spread of infections within the camps.</p>
+                                <h6>7. Health awareness, social mobilization, education, and advocacy 
+                                    activities:</h6>
+                            <p>Conducting community-based activities to raise awareness about disease control, general wellbeing, and health and nutrition. This includes the involvement of Community Health Workers 
+                                (CHWs) in delivering health education and promoting healthy practices.</p>
+
+
+                        </div>
+                        <div class="details-share">
+                            <div class="row">
+                                <div class="col-sm-6 col-lg-6">
+                                    <div class="left">
+                                        <ul>
+                                            <li>
+                                                <span>Share:</span>
+                                            </li>
+                                            <li>
+                                                <a href="#" target="_blank">
+                                                    <i class="icofont-facebook"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#" target="_blank">
+                                                    <i class="icofont-twitter"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#" target="_blank">
+                                                    <i class="icofont-youtube-play"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#" target="_blank">
+                                                    <i class="icofont-instagram"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-lg-6">
+                                    <div class="right">
+                                        <ul>
+                                            <li>
+                                                <span>Tags:</span>
+                                            </li>
+                                            <li>
+                                                <a href="#">#Donation</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">#Food</a>
+                                            </li>
+                                            <li>
+                                                <a href="#">#Help</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="details-payment">
+                            <h3>Select payment method</h3>
+                            <form>
+                                <div class="form-radio-area">
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                            id="inlineRadio1" value="option1">
+                                        <label class="form-check-label" for="inlineRadio1">Paypal</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                            id="inlineRadio2" value="option2">
+                                        <label class="form-check-label" for="inlineRadio2">Debit Card</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                            id="inlineRadio3" value="option3">
+                                        <label class="form-check-label" for="inlineRadio3">Credit Card</label>
+                                    </div>
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                            id="inlineRadio4" value="option4">
+                                        <label class="form-check-label" for="inlineRadio4">Other</label>
+                                    </div>
+                                </div>
+                                <div class="form-input-area">
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-user-alt-3"></i>
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="First name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-user-alt-3"></i>
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="Last name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-ui-email"></i>
+                                        </label>
+                                        <input type="email" class="form-control" placeholder="Email">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-ui-call"></i>
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="Phone number">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>
+                                            <i class="icofont-dollar"></i>
+                                        </label>
+                                        <input type="text" class="form-control" placeholder="$100.00">
+                                    </div>
+                                    <div class="text-center">
+                                        <button type="submit" class="btn common-btn">Donate Now</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 </div>
